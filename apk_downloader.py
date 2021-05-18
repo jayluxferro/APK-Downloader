85fbb13539d2f7b0d532aeefc937396a00e6254e
Jy <jayluxferro@sperixlabs.org>
2021-05-18 22:45:54 +0000

    Detect VPN or Proxy

--- apk_downloader.py
+++ apk_downloader.py
@@ -15,17 +15,16 @@ import ssl
 base_url = 'https://apps.evozi.com/apk-downloader'
 download_url = 'https://api-apk.evozi.com/download'
 
-payload='{}={}&{}={}&{}={}&fetch=false'
+payload = {}
 
 headers = {
-  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:87.0) Gecko/20100101 Firefox/87.0',
+  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15',
   'Accept': 'application/json, text/javascript, */*; q=0.01',
   'Accept-Language': 'en-US,en;q=0.5',
   'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
   'Origin': 'https://apps.evozi.com',
-  'DNT': '1',
   'Connection': 'keep-alive',
-  'Referer': 'https://apps.evozi.com/'
+  'Referer': 'https://apps.evozi.com/apk-downloader/'
 }
 
 def default_log():
@@ -51,7 +50,11 @@ if web_data.status_code == 200:
     token2 = res[164].strip().split('=')[-1].strip().replace("'", '').replace(';', '')
     token3 = res[195].strip().split('=')[-1].strip().replace(' ', '').strip('{').split(',')[:-1]
 
-    payload = payload.format(token3[0].split(':')[0], token1, token3[1].split(':')[0], bundle_identifier, token3[2].split(':')[0], token2)
+    payload[token3[0].split(':')[0]] = token1
+    payload[token3[1].split(':')[0]] = bundle_identifier
+    payload[token3[2].split(':')[0]] = token2
+    payload['fetch'] = False
+
     res = requests.post(download_url, data=payload, headers=headers)
     try:
         res = res.json()
@@ -68,4 +71,6 @@ if web_data.status_code == 200:
     except:
         default_log()
 else:
+    if web_data.text.find('Cloudflare') != -1:
+        print('Disable VPN or Proxy')
     default_log()
